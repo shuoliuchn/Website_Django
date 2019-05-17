5638241d92780a8c8b7a26c06b1379dc6fd9bae6
shuoliuchn <shuoliuchn@users.noreply.github.com>
2019-05-17 00:34:13 -0400

    first try

---
+++ Static/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# coding=utf-8
+
+#__author__ = "Shuo Liu"
+#__date__ = 2019-05-16 22:53
---
+++ wd/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WdConfig(AppConfig):
+    name = 'wd'
